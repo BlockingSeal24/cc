7de01f4e1e943b35188b603831dfe7612b57ef30
BlockingSeal24 <91137260+BlockingSeal24@users.noreply.github.com>
2025-12-30 00:17:07 -0500

    Added wishlist file to seperate all tracked SKUs and links.

---
+++ src/wishlist.js
@@ -0,0 +1,83 @@
+//Wishlist of items to track, "targetURL" is link to product page, "sku" is the identifier for the item
+const wishlist = [
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268151/asus-prime-geforce-rtx-5080-16gb-gddr7-prime-rtx5080-16g.html",
+      "sku": "MSRP ASUS Prime"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268141/gigabyte-geforce-rtx-5080-windforce-oc-sff-16g-graphics-card-geforce-rtx-5080-windforce-oc-sff-16g.html",
+      "sku": "MSRP Gigabyte Windforce OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268152/zotac-gaming-geforce-rtx-5080-solid-16gb-gddr7-zt-b50800d-10p.html",
+      "sku": "MSRP ZOTAC Solid"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268148/msi-geforce-rtx-5080-16g-ventus-3x-oc-plus-rtx-5080-16g-ventus-3x-oc-plus.html",
+      "sku": "$1,700 MSI Ventus"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268153/zotac-gaming-geforce-rtx-5080-solid-oc-16gb-gddr7-zt-b50800j-10p.html",
+      "sku": "$1,700 ZOTAC Solic OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267147/asus-prime-geforce-rtx-5080-16gb-gddr7-oc-edition-sff-prime-rtx5080-o16g.html",
+      "sku": "$1,739 ASUS Prime OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268138/gigabyte-geforce-rtx-5080-gaming-oc-ai-gaming-graphics-card-gv-n5080gaming-oc-16gd.html",
+      "sku": "$1,759 Gigabyte Gaming OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268147/msi-geforce-rtx-5080-16g-gaming-trio-oc-rtx-5080-16g-gaming-trio-oc.html",
+      "sku": "$1,799 MSI Gaming Trio OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267621/msi-geforce-rtx-5080-16g-vanguard-soc-launch-edition-rtx-5080-16g-vanguard-soc-launch-editio.html",
+      "sku": "$1,849 MSI Vanguard Launch"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267620/msi-geforce-rtx-5080-16g-suprim-soc-gddr7-16gb-rtx-5080-16g-suprim-soc.html",
+      "sku": "$1,879 MSI Suprim OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268144/gigabyte-aorus-geforce-rtx-5080-master-16g-gddr7-gv-n5080aorus-m-16gd.html",
+      "sku": "$1,899 Gigabyte Aorus Master"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267619/msi-geforce-rtx-5080-16g-suprim-liquid-soc-gddr7-16gb-rtx-5080-16g-suprim-liquid-soc.html",
+      "sku": "$1,949 MSI Suprim Liquid"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267146/asus-tuf-gaming-geforce-rtx-5080-16gb-gddr7-oc-edition-tuf-rtx5080-o16g-gaming.html",
+      "sku": "$1,959 Asus TUF Gaming"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268136/gigabyte-aorus-geforce-rtx-5080-xtreme-waterforce-aio-cooling-gv-n5080aorusx-w-16gd.html",
+      "sku": "$2,049 Gigabyte Aorus AIO"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/267145/asus-rog-astral-geforce-rtx-5080-16gb-gddr7-oc-edition-quad-fan-force-rog-astral-rtx5080-o16g-gaming.html",
+      "sku": "$2,179 ASUS Astral 4-Fan"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268191/gigabyte-geforce-rtx-5090-windforce-oc-32g-gddr7-graphics-card-gv-n5090wf3oc-32gd.html",
+      "sku": "5090 MSRP Gigabyte Windforce OC"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268262/asus-tuf-gaming-geforce-rtx-5090-32gb-gddr7-tuf-rtx5090-32g-gaming.html",
+      "sku": "5090 MSRP ASUS TUF Gaming"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268283/zotac-gaming-geforce-rtx-5090-solid-32gb-gddr7-zt-b50900d-10p.html",
+      "sku": "5090 $3,100 ZOTAC Solid"
+    },
+    {
+      "targetURL": "https://www.canadacomputers.com/en/powered-by-nvidia/268284/zotac-gaming-geforce-rtx-5090-solid-32gb-gddr7-zt-b50900j-10p.html",
+      "sku": "5090 $3,300 ZOTAC Solid OC"
+    },
+  ];
+
+module.exports = {
+    wishlist
+};
